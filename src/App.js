0c799d6b5f9cccbf9cef1fd51b850059fd4a4497
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-05-13 21:16:54 +0500

    Use Effect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import UseStatePractice from './Components/UseStatePractice';
+import UseEffectPractice from './Components/UseEffectPractice';
 
 function App() {
   return (
     <div className="App">
+
 <UseStatePractice />
 <hr />
+<UseEffectPractice />
     </div>
   );
 }
--- src/Components/UseEffectPractice.js
+++ src/Components/UseEffectPractice.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import UseStatePractice from './UseStatePractice'
+
 
 function UseEffectPractice() {
+// It will run on every render 
+  useEffect( ()=>{
+  alert("I am running on every Render")
+})
+// It will run only first time when you will open your website 
+
+useEffect( ()=>{
+  alert("I will run only first time of web page")
+},[])
+
   return (
     <div>
-      
+<UseStatePractice />
+
     </div>
   )
 }
